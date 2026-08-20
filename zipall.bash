5ff76b3b83f0390c37a449314b9a62f9097580b7
NeoriGG <cheburashek444@gmail.com>
2026-08-20 21:34:35 +0300

    Delete zipall.bash

--- zipall.bash
+++
@@ -1,3 +0,0 @@
-zip -r src/all.zip src/clear.c src/lsdr.c src/dog.c src/echo.c src/whereami.c src/time.c
-zip -r files/all.zip files/clear files/lsdr files/dog files/echo files/whereami files/time
-zip -r static/all.zip static/clear static/lsdr static/dog static/echo static/whereami static/time
